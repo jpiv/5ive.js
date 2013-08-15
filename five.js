13ef27e1d39b16912557486553f3c1a036815818
Fred Zirdung <fred@hackreactor.com>
2013-08-14 21:47:53 -0700

    Fix spelling error.

--- five.js
+++ five.js
@@ -3,7 +3,7 @@ Fivejs = {};
 Fivejs.five = function(num) {
   try {
     if (typeof num !== 'number') {
-      throw "invalid input to FiveJs.five() " + num + ' is not a mnumber.';
+      throw "invalid input to FiveJs.five() " + num + ' is not a number.';
     } else {
       return (num*5/5 ) + 5 - 5;
     }
